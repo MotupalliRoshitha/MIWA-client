df907633b36d22b19d53ff22f34feb14a5269447
sudarshan <thirusudarshan@gmail.com>
2024-05-30 00:45:23 +0530

    add search

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Box, Paper, TextField, useMediaQuery } from "@mui/material";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  const search = (e) => {
+    if (e.key === "Enter")
+    console.log(e.target.value);
+  }
+
+  const isNonMobile = useMediaQuery('(min-width:600px)')
+
+
+  return (
+      <Paper>
+        <Box height="100vh" width="100vw" display="flex" alignItems="center" justifyContent="center" flexDirection="column" paddingTop="125px" >
+          <Box  width={isNonMobile ? "40%" : "80%"} display="flex" justifyContent="center" sm={{width: "100%"}}>
+            <TextField id="search" label="Type your movie name and press enter to search ..." fullWidth onKeyDown={(e) => search(e)} />
+          </Box>
+
+
+        </Box>
+      </Paper>
+  );
+};
+
+export default Home;
